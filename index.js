ee0ec77e7a7031de1768c08e31444b6ca7f81041
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-02-13 20:50:51 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+let saveData;
+function loadSaveFile() {
+    let saveFileReader = new FileReader();
+    saveFileReader.readAsArrayBuffer(saveFile.files[0]);
+    saveData = new Uint8Array(saveFileReader.result);
+}
