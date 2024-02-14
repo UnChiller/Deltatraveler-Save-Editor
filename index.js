9cda25ecfe2a6dedafbaafd4d00ee673cd4457bb
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-02-14 10:24:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ function loadSaveFile() {
     let saveFileReader = new FileReader();
     saveFileReader.onload = (loader => {
         let saveData = new Uint8Array(loader.target.result);
-        loadMessage.innerText = "Successfully loaded save";
-        setTimeout(() => {loadMessage.innerText = ""});
-        processSaveFile(saveData);
+        loadMessage.innerText = "Loading...";
+        if (processSaveFile(saveData)) {
+            loadMessage.innerText = "Successfully loaded save";
+            setTimeout(() => {loadMessage.innerText = ""});
+        } else {
+            loadMessage.innerText = "failed to load save";
+        }
     });
     saveFileReader.readAsArrayBuffer(saveFile.files[0]);
 }
 
 function processSaveFile(saveData) {
     console.log(saveData);
+    return true;
 }
 
 submitFile.onclick = loadSaveFile;
