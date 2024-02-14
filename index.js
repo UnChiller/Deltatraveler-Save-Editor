c6fdc7a9fc3f07aaca28361f3a5d981ef5c4e74e
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-02-14 09:34:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function loadSaveFile() {
     let saveFileReader = new FileReader();
     saveFileReader.readAsArrayBuffer(saveFile.files[0]);
     saveData = new Uint8Array(saveFileReader.result);
+    console.log(saveFileReader);
 }
 submitFile.onclick = loadSaveFile;
