03e0a0bd6ac5816d3f4aa5da95a2c2fcc14e87e4
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-10-31 18:14:46 -0500

    dev scripts update

--- configure.sh
+++ configure.sh
@@ -1,8 +1,6 @@
 cd src
 rm -r node_modules
-rm package.json package-lock.json
-echo "{}" > package.json
-npm install --save-dev react react-dom vite
-npm install --save-dev @types/react @types/react-dom @types/node
-sudo npm install -g vite typescript
+rm package-lock.json
+npm install
+sudo npm install -g vite
 cd ..
